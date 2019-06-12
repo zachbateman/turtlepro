49d1c701f0cb49fa0734604b0fcc90fc243c3e0b
Zach Bateman <zkbateman@gmail.com>
2019-06-11 23:50:57 -0500

    Add PolarTurtle (subclass of TurtlePro)

--- turtlepro/controller.py
+++ turtlepro/controller.py
@@ -34,3 +34,48 @@ class TurtlePro():
         OF DRAWING so that TurtlePro knows how far to go!
         '''
         self.write_text(text, 0, self.max_dist_from_center + 5, size=16)
+
+
+
+def PolarTurtle(TurtlePro):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.max_radius = 0
+
+
+    def circle_xy(self, radians, radius) -> tuple:
+        '''Calc coords of edge of circle at given angle'''
+        return (math.cos(radians) * radius, math.sin(radians) * radius)
+
+    def _check_radius(self, radius):
+        '''Used to capture maximum radius used for drawing'''
+        if radius > self.max_radius:
+            self.max_radius = radius
+
+    def set_title(self, text) -> None:
+        '''Put a title at the top of the drawing.  Do this AT THE END
+        OF DRAWING so that TurtlePro knows how far to go!
+        '''
+        self.write_text(text, 0, self.max_radius + 5, size=16)
+
+    def draw_ark(self, init_radian, end_radian, radius, fast=False) -> None:
+        '''Draw an ark (part of a circle'''
+        self._check_radius(radius)
+        turtle.penup()
+        turtle.goto(*self.circle_xy, init_radian, end_radian, radius)
+        turtle.pendown()
+        num_segments=40 if not fast else 3
+        for angle in range(1, num_segments + 1):
+            turtle.goto(*self.circle_xy(init_radian + (end_radian - init_radian) * (angle / num_segments), radius))
+
+    def draw_circle(self, radius) -> None:
+        '''Draw a circle of given radius'''
+        self.draw_ark(0, 2 * math.pi, radius)
+
+    def draw_polarline(self, angle, radius_1, radius_2) -> None:
+        '''Draw a line directly out from center with endpoints specified by radii'''
+        turtle.penup()
+        turtle.goto(*self.circle_xy(angle, radius_1))
+        turtle.pendown()
+        turtle.goto(*self.circle_xy(angle, radius_2))
